dc3336f45f43c34808cf81eba0e2979edbf94a16
michaelgeorgescu <georgescu.michael@gmail.com>
2022-09-17 19:24:53 -0400

    strict mode to fix some bug

--- NFT-Website/src/index.js
+++ NFT-Website/src/index.js
@@ -4,8 +4,8 @@ import './styles/index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React>
+  <React.StrictMode>
     <App />
-  </React>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
